f7f8b09d1a97433d20459515793ab1812e854f43
ParnwadKonthong <parnwad.konthong@gmail.com>
2025-05-27 17:59:04 +0700

    home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,11 @@
 <template>
-    <div>Home</div>
+  <div class="flex flex-col items-center justify-center mt-20">
+    <div class="text-center">
+      <div class="text-3xl">Hello, I'm</div>
+      <div class="font-bold text-6xl">PARNWAD KONTHONG</div>
+    </div>
+    <div class="">
+      <img src="../assets/img-home.png" />
+    </div>
+  </div>
 </template>
