987b25886bf1655b67c353d0fcefa10b5485c319
ParnwadKonthong <parnwad.konthong@gmail.com>
2025-05-29 15:54:06 +0700

    contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import SkillItem from '../components/SkillItem.vue';
+</script>
+
 <template>
-    <div>Contact</div>
+    <div class="flex flex-col items-center justify-center my-10">
+        <div class="font-bold text-4xl mb-12">CONTACT ME</div>
+
+        <div class="flex flex-wrap justify-center gap-4">
+            <SkillItem icon="../public/logo-mail.png" name="parnwad.konthong@gmail.com" />
+            <SkillItem icon="../public/logo-github.png" name="ParnwadKonthong" />
+            <SkillItem icon="../public/logo-phone.png" name="097-072-6523" />
+        </div>
+        
+    </div>
 </template>
